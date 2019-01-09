234d2b3e5dd007c436ed9eb511cdc08aa9411844
Philippe <Philippe.Trebuchet@ssi.gouv.fr>
2019-01-09 08:44:15 +0100

    memsetting device structure

--- spi_generic.h
+++ spi_generic.h
@@ -83,6 +83,7 @@ uint8_t spi_early_init()
      * This function create a device_t, fullfill it, and execute a
      * sys_init(INIT_DEVACCESS) syscall.
      */
+    memset(&dev,sizeof(device_t),0);
     memcpy(dev.name, spiname, strlen(spiname));
     dev.address = SPIBASE;
     dev.size = 0x400; /*FIXME: 0x400????? just need 0x20 */
